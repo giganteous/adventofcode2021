1d7be22849a557acab19580cf5d66c1eb27e06f4
Kai Storbeck <kai@xs4all.nl>
2021-12-01 12:08:24 +0100

    pandas version

--- day1/part2.py
+++ day1/part2.py
@@ -1,33 +1,10 @@
 #!/usr/bin/python
 
-import os, sys
 import pandas as pd
-import numpy as np
 
-infile = open('input.csv', 'r')
-lines = infile.readlines()
-
-alist = []
-lastm = 0
-increases = 0
-for l in lines[1:]:
-    l = int(l.strip('\n'))
-    alist.append(l)
-
-s = pd.Series(alist).astype("Int64")
-lastreading = np.NaN
-increases=0
-for element in s.rolling(window=3).sum():
-    #if element == np.NaN:
-    #elif lastreading == np.NaN:
-    #if lastreading > element: print(element, ' (decrease)')
-    if lastreading < element:
-        #print(element, ' (increase)')
-        increases += 1
-    #else:
-        #print(element, ' (steady)')
-        #print(element, 'not enough readings (or no last, or steady)')
-    lastreading = element
-
-print('total increases: ', increases)
+df = pd.read_csv('input.csv')
+df["r"] = df.rolling(3).x.sum() # rolling window
+#print('total increases: ', len(df[df.diff() > 0]))
+df["difference"] = df.r.diff()  # difference with previous row
+print('total increases: ', len(df[df.difference > 0]))
 
