09f7ba663d31b7c074368eaa4c42b78e8a0a99ce
Kai Storbeck <kai@xs4all.nl>
2021-12-01 12:11:27 +0100

    easier with pandas

--- day1/part1.py
+++ day1/part1.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python
 
-import os, sys
+import pandas as pd
+df = pd.read_csv('input.csv')
+df["difference"] = df.x.diff()
 
-infile = open('input.csv', 'r')
-lines = infile.readlines()
-
-lastm = 0
-increases = 0
-for l in lines[1:]:
-    l = int(l.strip('\n'))
-    if l > lastm:
-        increases += 1
-    lastm = l
-
-print('total increases: ', increases)
+print('total increases: ', len(df[df.difference > 0]))
 
 
